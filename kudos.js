db48c53613bfd4db82b35bea28042a7dea60c3ad
Otho TK <otho.tk@hotmail.com>
2021-06-27 15:40:19 -0300

    :sparkles: getKudosForUser 1st version

--- kudos.js
+++ kudos.js
@@ -20,13 +20,37 @@ const KUDOS_TO_REAL = [
   Recebe: um int representando o número de pontos do usuário
   Retorna: um array contendo os kudos. Ex.: ['OK', 'GOOD'] 
 */
-function getKudosForUser(points) {}
+function getKudosForUser(points) {
+  let kudo2pointsReversed = [...KUDOS_TO_POINTS]
+  kudo2pointsReversed = kudo2pointsReversed.reverse()
+
+  // Express how many kudos will be required to 
+  // a certain amount of points
+  var kudoHistogram = []
+  
+  // For each Kudo, computes how many kudos 
+  // is used for the remaining amount of points
+  kudo2pointsReversed.forEach((kudoObj,index)=>{
+    let times = Math.floor(points/kudoObj.value)
+    kudoHistogram.push(times)
+    points = points-(times*kudoObj.value)
+  })
+
+  // Given the kudos amount, an array with the 
+  // given amount lenght is filled with the
+  // correspondent kudo
+  let kudo =  kudoHistogram.map((freq, index)=>
+    Array(freq).fill(kudo2pointsReversed[index])
+  ).flat()
+
+  return kudo
+}
 
 /* 
   Recebe: Recebe um array contendo os nomes dos kudos de um usuário. Ex.: ['OK', 'GOOD']
   Retorna: a mensagem padrão com o valor em reais dos kudos por extenso. Ex.: Parabéns, você ganhou vinte e cinco reais
 */
-function getKudosValueMessageForUser(kudos) {}
+function getKudosValueMessageForUser(kudos) { }
 
 module.exports = {
   getKudosForUser,
